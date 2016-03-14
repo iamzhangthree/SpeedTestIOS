7df6707e2bc09a7edb66aad2fe1fdcd424fdb095
iamzhangthree <zhangthree1988@163.com>
2016-03-15 00:10:02 +0800

    Kick off the project!

---
+++ TestSpeedIOS/TestSpeedIOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TestSpeedIOS
+//
+//  Created by zhangthree on 3/15/16.
+//  Copyright © 2016 zhangletao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TestSpeedIOS/TestSpeedIOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TestSpeedIOS
+//
+//  Created by zhangthree on 3/15/16.
+//  Copyright © 2016 zhangletao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
